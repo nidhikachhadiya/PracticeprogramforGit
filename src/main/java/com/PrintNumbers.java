3f08530baa482cc34e76f4996b916388750a24c5
Ravi Jiyani <jiyaniravi@gmail.com>
2023-10-21 21:11:35 -0400

    Add program to print numbers

---
+++ src/main/java/com/PrintNumbers.java
@@ -0,0 +1,17 @@
+package com;
+
+public class PrintNumbers {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i<=50) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+
+}
